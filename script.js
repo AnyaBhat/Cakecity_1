09f3a04e4c096dbb6f440067e7f109e10c28e7c9
Ananya Bhat <anyabhat18@gmail.com>
2023-08-26 10:36:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Script for navigation bar
+const bar=document.getElementById('drawer');
+const nav=document.getElementById('navbar');
+
+
+if (bar){
+    bar.addEventListener('click',()=>{
+        nav.classList.add('active');
+    });
+}
